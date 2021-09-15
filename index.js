ee901889cf8d5b1d06119fb0054ff005765cbabf
Yasha Gozwan Shuhada <62646078+yashagozwan@users.noreply.github.com>
2021-09-16 02:01:38 +0700

    refactor and print person to the console

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-const person = [
+// persons list
+const persons = [
   { name: 'yasha', age: 24 },
   { name: 'serizawa', age: 21 },
   { name: 'fumina', age: 33 },
   { name: 'sari', age: 27 },
   { name: 'isabella', age: 35 },
 ]
+
+// print to the console
+persons.forEach(person => {
+  console.log(person.name)
+})
