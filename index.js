cd74e77676cb7b7de2789660b1e3dcb0d69c265d
Yasha Gozwan Shuhada <62646078+yashagozwan@users.noreply.github.com>
2021-09-16 01:59:36 +0700

    adding new persons

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const person = {
-  name: 'yasha',
-  age: 24
-}
+const person = [
+  { name: 'yasha', age: 24 },
+  { name: 'serizawa', age: 21 },
+  { name: 'fumina', age: 33 },
+  { name: 'sari', age: 27 },
+  { name: 'isabella', age: 35 },
+]
